b439ddebec394b5cac71e0a0852c1b68d33d83bb
zxxmengmei <157577331@qq.com>
2016-12-20 14:30:53 +0800

    commit

--- Pods/Target Support Files/Pods-ChallengeHighSalary/Pods-ChallengeHighSalary-frameworks.sh
+++ Pods/Target Support Files/Pods-ChallengeHighSalary/Pods-ChallengeHighSalary-frameworks.sh
@@ -88,7 +88,6 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/HandyJSON/HandyJSON.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MJRefresh/MJRefresh.framework"
   install_framework "$BUILT_PRODUCTS_DIR/PagingMenuController/PagingMenuController.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/PickerView/PickerView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
@@ -96,6 +95,5 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/HandyJSON/HandyJSON.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MJRefresh/MJRefresh.framework"
   install_framework "$BUILT_PRODUCTS_DIR/PagingMenuController/PagingMenuController.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/PickerView/PickerView.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SDWebImage/SDWebImage.framework"
 fi
